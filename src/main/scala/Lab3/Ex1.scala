e1cf608bbf594d28af13a2b5569a7509c8a9c8c1
Razvan989 <hs.razvan4@gmail.com>
2020-10-15 08:46:48 +0200

    Lab3

--- src/main/scala/Lab3/Ex1.scala
+++ src/main/scala/Lab3/Ex1.scala
@@ -1,15 +1,44 @@
 package Lab3 
+// febra 6%  tusesti 4%
+// febra + tuse -> covid
+//5% din pop inf
+//confirmat covid -> care e sansa sa faci febra/tuse ?
+//nu aveti nici febra nici tuse care e sansa sa fii asimptomatic
 
+import com.cra.figaro.algorithm.factored._
 import com.cra.figaro.language._
-import com.cra.figaro.algorithm.sampling._
-
+import com.cra.figaro.library.compound._
 object Ex1 {
-	def main(args: Array[String]) {
-		val test = Constant("Test")
+	Universe.createNew()
+	private val febra = Flip(0.06)
+	private val tuse = Flip(0.04)
 
-		val algorithm = Importance(1000, test)
-		algorithm.start()
+	private val alarm = CPD(febra, tuse,
+		(false, false) -> Flip(0.001),
+		(false, true) -> Flip(0.1),
+		(true, false) -> Flip(0.7),
+		(true, true) -> Flip(0.98))
+
+
+	private val Covid = CPD(alarm,     false -> Flip(0.01),     true -> Flip(0.7))
+	def main(args: Array[String]): Unit =
+	{
+		//Confirmat covid ->  sansa sa ai febra / tuse
 		
-		println(algorithm.probability(test, "Test"))
+		Covid.observe(true)
+		val alg = VariableElimination(febra, tuse)
+		alg.start()
+		println("Probabilitatea sa ai febra : " + alg.probability(febra, true))
+		println("Probabilitatea sa tusesti : " + alg.probability(tuse, true))
+		alg.kill
+
+		//Nu ai tuse / febra -> sansa sa fii asimptomatic
+		febra.observe(false)
+		tuse.observe(false)
+		val alg2 = VariableElimination(Covid)
+		alg2.start()
+		println("Probabilitatea sa ai covid (asimptomatic) : " + alg2.probability(Covid, true))
+		alg2.kill
 	}
+
 }
