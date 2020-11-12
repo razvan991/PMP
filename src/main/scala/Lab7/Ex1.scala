7222ebeb7a60d05589dbc14ad773367be7098c89
Razvan989 <hs.razvan4@gmail.com>
2020-11-12 10:42:58 +0200

    Lab 7

--- src/main/scala/Lab7/Ex1.scala
+++ src/main/scala/Lab7/Ex1.scala
@@ -1,15 +1,35 @@
 package Lab7
 
+import Lab7.golf.greaterThan80
 import com.cra.figaro.language._
 import com.cra.figaro.algorithm.sampling._
-
+import com.cra.figaro.library.atomic.discrete.Uniform
+import com.cra.figaro.library.collection.Container
+object golf{
+	def greaterThan80(score: Double): Boolean = {
+		if(score > 80.0)
+			return true
+		return false
+	}}
 object Ex1 {
 	def main(args: Array[String]) {
-		val test = Constant("Test")
+		var par = Array(3.0, 3.0, 4.0, 3.0, 4.0, 4.0, 5.0, 4.0, 5.0, 3.0, 4.0, 5.0, 5.0, 3.0, 4.0, 4.0, 4.0, 5.0)
 
-		val algorithm = Importance(1000, test)
-		algorithm.start()
-		
-		println(algorithm.probability(test, "Test"))
+		var skill = Uniform(0, 8.0/13.0)
+
+		var shots = List.tabulate(18)(n => Select(
+			Apply(skill, (skill: Double) => skill/8) -> (par(n) - 2),
+			Apply(skill, (skill: Double) => skill/2) -> (par(n) - 1),
+			Apply(skill, (skill: Double) => skill) -> (par(n)),
+			Apply(skill, (skill: Double) => 4.0/5.0 * (1 - 13.0*skill/8.0)) -> (par(n) + 1),
+			Apply(skill, (skill: Double) => 1.0/5.0 * (1 - 13.0*skill/8.0)) -> (par(n) + 2)))
+		// punctul a
+		val score: Element[Double] = Container(shots:_*).reduce(_ + _)
+		val alg = Importance(1000, score)
+		alg.start()
+		println("Probabilitatea ca scorul sa fie mai mare decat 80: ")
+		println(alg.probability(score, greaterThan80 _))
+		alg.stop()
+
 	}
 }
