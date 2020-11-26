56f45f434561b36567022c34cb6ae8817b2357fd
Razvan989 <hs.razvan4@gmail.com>
2020-11-26 09:34:18 +0200

    Lab 8

--- src/main/scala/Lab8/Ex1.scala
+++ src/main/scala/Lab8/Ex1.scala
@@ -2,14 +2,29 @@ package Lab8
 
 import com.cra.figaro.language._
 import com.cra.figaro.algorithm.sampling._
+class Departments{
+	val health=Select(0.5->"healthy",0.5->"unhealthy")
+}
+abstract class researchAndDevelopment extends Departments {
+	val budget:Element[Int]
+}
+abstract class production extends Departments {
+	val product:Element[String]
+}
+abstract class sales extends Departments {
+	val identifyClient:Element[Boolean]
+}
+abstract class humanResources extends Departments {
+	val promotingEmployee:Element[Boolean]
+	val recruitEmployee:Element[Boolean]
+}
+abstract class Finance extends Departments {
+	val funds:Element[Int]
+	val profit:Element[Int]
+}
 
 object Ex1 {
 	def main(args: Array[String]) {
-		val test = Constant("Test")
 
-		val algorithm = Importance(1000, test)
-		algorithm.start()
-		
-		println(algorithm.probability(test, "Test"))
 	}
 }
